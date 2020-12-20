9a05df24b731c2e7d33a8554753bdfe634bb57c6
makaulucky <makaulucky5@gmail.com>
2020-12-20 09:30:10 +0300

    Social link (Twitter) modified

--- footer.php
+++ footer.php
@@ -2,5 +2,5 @@
 
 
 <div class="footer-wrap pd-20 mb-20 card-box">
-			Nyumbani Village CCC -  By <a href="https://github.com/makaulucky" target="_blank">Lucky Makau</a>
+			Nyumbani Village CCC System -  By <a href="https://twitter.com/makaulucky/" target="_blank">Lucky Makau</a>
 			</div>
