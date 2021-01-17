9ffcf832fb5c6bab3faf33dedb1243a1b624e833
makaulucky <makaulucky5@gmail.com>
2021-01-17 14:08:53 +0300

    Search icon removed

--- navigation.php
+++ navigation.php
@@ -1,14 +1,9 @@
 
 //header start here
 	<div class="header">
-
-
 		<div class="header-left">
-			<div class="menu-icon dw dw-menu"></div>
-			<div class="search-toggle-icon dw dw-search2" data-toggle="header_search"></div>
+			<div class="menu-icon dw dw-menu"></div>		
 		</div>
-
-
 
 		<div class="header-right">
 			<div class="dashboard-setting user-notification">
