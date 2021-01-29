309578fba4eb010ca852167b5228bb56ff6f830e
makaulucky <makaulucky20@gmail.com>
2021-01-29 17:25:38 +0300

    Some code modificaton

--- forgot-password.php
+++ forgot-password.php
@@ -37,9 +37,98 @@ session_start();
 							<h2 class="text-center text-primary">Forgot Password</h2>
 						</div>
 						<h6 class="mb-20">Enter your email address to reset your password</h6>
-						<form>
+						
+						
+						<?php
+include('dbconfig.php');
+if(isset($_POST["email"]) && (!empty($_POST["email"]))){
+$email = $_POST["email"];
+$email = filter_var($email, FILTER_SANITIZE_EMAIL);
+$email = filter_var($email, FILTER_VALIDATE_EMAIL);
+if (!$email) {
+   $error .="<p>Invalid email address please type a valid email address!</p>";
+   }else{
+   $sel_query = "SELECT * FROM user_reg WHERE email=".$email."";
+   $results = mysqli_query($con,$sel_query);
+   $row = mysqli_num_rows($results);
+   if ($row==""){
+   $error .= "<p>No user is registered with this email address!</p>";
+   }
+  }
+   if($error!=""){
+   echo "<div class='error'>".$error."</div>
+   <br /><a href='javascript:history.go(-1)'>Go Back</a>";
+   }else{
+   $expFormat = mktime(
+   date("H"), date("i"), date("s"), date("m") ,date("d")+1, date("Y")
+   );
+   $expDate = date("Y-m-d H:i:s",$expFormat);
+   $key = md5(2418*2+$email);
+   $addKey = substr(md5(uniqid(rand(),1)),3,10);
+   $key = $key . $addKey;
+// Insert Temp Table
+mysqli_query($con,
+"INSERT INTO `password_reset_temp` (`email`, `key`, `expDate`)
+VALUES ('".$email."', '".$key."', '".$expDate."');");
+ 
+$output='<p>Dear user,</p>';
+$output.='<p>Please click on the following link to reset your password.</p>';
+$output.='<p>-------------------------------------------------------------</p>';
+$output.='<p><a href="https://172.16.1.254/clinic/reset-password.php?
+key='.$key.'&email='.$email.'&action=reset" target="_blank">
+https://172.16.1.254/clinic/reset-password.php
+?key='.$key.'&email='.$email.'&action=reset</a></p>'; 
+$output.='<p>-------------------------------------------------------------</p>';
+$output.='<p>Please be sure to copy the entire link into your browser.
+The link will expire after 1 day for security reason.</p>';
+$output.='<p>If you did not request this forgotten password email, no action 
+is needed, your password will not be reset. However, you may want to log into 
+your account and change your security password as someone may have guessed it.</p>';   
+$output.='<p>Thanks,</p>';
+$output.='<p>AllPHPTricks Team</p>';
+$body = $output; 
+$subject = "Password Recovery - Nyumbani Village";
+ 
+$email_to = $email;
+$fromserver = "noreply@yourwebsite.com"; 
+require_once('phpmail/PHPMailerAutoload.php');
+ 
+    $mail = new PHPMailer();
+ 
+    $mail->CharSet =  "utf-8";
+    $mail->IsSMTP();
+    // enable SMTP authentication
+    $mail->SMTPAuth = true;                  
+    // GMAIL username
+    $mail->Username = "makaulucky5@gmail.com";
+    // GMAIL password
+    $mail->Password = "Lmakau@5!";
+    $mail->SMTPSecure = "ssl";  
+    // sets GMAIL as the SMTP server
+    $mail->Host = "smtp.gmail.com";
+    // set the SMTP port for the GMAIL server
+    $mail->Port = "465";
+    $mail->From='makaulucky5@gmail.com';
+    $mail->FromName='Lucky Makau';
+$mail->Sender = $fromserver; // indicates ReturnPath header
+$mail->Subject = $subject;
+$mail->Body = $body;
+$mail->AddAddress($email_to);
+if(!$mail->Send()){
+echo "Mailer Error: " . $mail->ErrorInfo;
+}else{
+echo "<div class='error'>
+<p>An email has been sent to you with instructions on how to reset your password.</p>
+</div><br /><br /><br />";
+ }
+   }
+}else{
+?>
+<?php } ?>			
+					
+							<form method="POST" action="" name="reset">
 							<div class="input-group custom">
-								<input type="text" class="form-control form-control-lg" placeholder="Email">
+								<input type="email" name="email" class="form-control form-control-lg" placeholder="Email">
 								<div class="input-group-append custom">
 									<span class="input-group-text"><i class="fa fa-envelope-o" aria-hidden="true"></i></span>
 								</div>
@@ -51,7 +140,7 @@ session_start();
 											use code for form submit
 											<input class="btn btn-primary btn-lg btn-block" type="submit" value="Submit">
 										-->
-										<a class="btn btn-primary btn-lg btn-block" href="index.php">Submit</a>
+										<a class="btn btn-primary btn-lg btn-block" name="password_reset_temp"  type="submit" value="submit">Submit</a>
 									</div>
 								</div>
 								<div class="col-2">
@@ -64,6 +153,9 @@ session_start();
 								</div>
 							</div>
 						</form>
+						<p>&nbsp;</p>
+						<p>&nbsp;</p>
+						<p>&nbsp;</p>
 					</div>
 				</div>
 			</div>
