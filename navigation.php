2a0e2fe62ce317ee91fda314d41341cc91037fea
makaulucky <makaulucky5@gmail.com>
2021-02-03 13:01:45 +0300

    Admissions names

--- navigation.php
+++ navigation.php
@@ -174,7 +174,7 @@
 						</a>
 						<ul class="submenu">
 							<li><a href="index.php">Home</a></li>
-							<li><a href="admission.php">Enroll</a></li>
+							<li><a href="admission.php">Admissions</a></li>
 							<li><a href="ccclist.php">Registration List</a></li>
 							<li><a href="ccctable.php">CCC Report</a></li>
 							<li><a href="labresults.php" >Results & History</a></li>
