30614004bc151b81f70302b67cb83e05ed56b1ee
makaulucky <makaulucky20@gmail.com>
2021-02-08 12:13:50 +0300

    Minor Change

--- footer.php
+++ footer.php
@@ -2,6 +2,6 @@
 
 
 <div class="footer-wrap pd-20 mb-20 card-box">
-			Nyumbani Village CCC System -  By <a href="https://twitter.com/makaulucky/" target="_blank">Lucky Makau</a> <br>
+			[ Nyumbani Village ] CCC System -  By <a href="https://twitter.com/makaulucky/" target="_blank">Lucky Makau</a> <br>
 			
 			</div>
