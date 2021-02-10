c303a233fc38423a8afc7d56bfedf9ba594dd710
makaulucky <makaulucky20@gmail.com>
2021-02-10 17:29:18 +0300

    Initial commit

---
+++ Database/agescript.sql
@@ -0,0 +1,35 @@
+-- Script to run Age-Range
+
+SELECT
+    CASE
+        WHEN age < 1 THEN 'Under 1'
+        WHEN age BETWEEN 1 and 4 THEN '1 - 4'
+        WHEN age >= 1 THEN '1 - 4'
+        WHEN age BETWEEN 5 and 9 THEN '5 - 9'
+        WHEN age >= 5 THEN '5 - 9'
+        WHEN age BETWEEN 10 and 14 THEN '10 - 14'
+        WHEN age >= 10 THEN '10 - 14'
+        WHEN age BETWEEN 15 and 19 THEN '15 - 19'
+        WHEN age >= 15 THEN '15 - 19'
+        WHEN age BETWEEN 20 and 24 THEN '20 - 24'
+        WHEN age >= 20 THEN '20 - 24'
+        WHEN age BETWEEN 25 and 29 THEN '25 - 29'
+        WHEN age >= 25 THEN '25 - 29'
+        WHEN age BETWEEN 30 and 34 THEN '30 - 34'
+        WHEN age >= 30 THEN '30 - 34'
+        WHEN age BETWEEN 35 and 39 THEN '35 - 39'
+        WHEN age >= 35 THEN '35 - 39'
+        WHEN age BETWEEN 40 and 44 THEN '40 - 44'
+        WHEN age >= 40 THEN '40 - 44'
+        WHEN age BETWEEN 45 and 49 THEN '45 - 49'
+        WHEN age >= 45 THEN '45 - 49'
+        WHEN age >= 50 THEN 'Over 50'
+        WHEN age IS NULL THEN 'Not Filled In (NULL)'
+    END as `years`,
+    COUNT(*) AS count
+
+    FROM (SELECT TIMESTAMPDIFF(YEAR, `Dob`, CURDATE()) AS Age FROM detailed_results) as derived
+
+    GROUP BY `years`
+
+    ORDER BY `years`
