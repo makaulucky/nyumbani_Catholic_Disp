5fdb8fd5753d2865067e0b84dc480ab9bd756919
makaulucky <makaulucky20@gmail.com>
2021-05-24 13:12:19 +0300

    Modified redirects

--- results_entry.php
+++ results_entry.php
@@ -270,9 +270,9 @@ $curr_vl_dateFromPre = date("d-M-Y", strtotime($curr_vl_dateFromPre));
 											<label>Line:</label>
 											<select name='mstari' class="custom-select form-control" required>
 											<option value="<?php echo "$mstari";?>"><?php echo "$mstari";?></option>
-												<option value="1">First Line</option>
-												<option value="2">Second Line</option>
-												<option value="3">Third Line</option>
+												<option value="First Line">First Line</option>
+												<option value="Second Line">Second Line</option>
+												<option value="Third Line">Third Line</option>
 												<option value="Exposed">Exposed</option>
 										</select>
 										</div>
